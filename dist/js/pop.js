2790af8287ab8416c0e4fa014f2513f5a07c2789
Andrew Pang <andrew.y.pang@gmail.com>
2019-07-16 14:13:24 -0700

    rearranging photos

--- dist/js/pop.js
+++ dist/js/pop.js
@@ -2,10 +2,7 @@ $(document).ready(function() {
 
     $('#lightboxModal').on('show.bs.modal', function (event) {
         var photo = $(event.relatedTarget) // Button that triggered the modal
-        console.log(photo);
-
         var paths = photo.data('photopath');
-        console.log(paths);
 
         // Clear Carousel - both indicators and inner
         var carouselIndicators = document.getElementById("carousel-indicators");
@@ -32,11 +29,9 @@ $(document).ready(function() {
 
 
                 // CREATE THE CAROUSEL ITEM
-                // First child get additional "active" class
 
                 var newCaroItem = document.createElement('div');
                 newCaroItem.className = 'carousel-item';
-                newCaroItem.setAttribute("data-interval", "false");
                 
                 var newImg = document.createElement('img');
                 newImg.src = pathForPhoto;
@@ -45,22 +40,13 @@ $(document).ready(function() {
                 newCaroItem.appendChild(newImg);
 
                 document.getElementById('carousel-inner').appendChild(newCaroItem);
-                // $('carousel-inner').appendChild(newCaroItem);
-
             });
 
             // First child get additional "active" class
             carouselIndicators.firstChild.className = carouselIndicators.firstChild.className + " active";
             carouselInner.firstChild.className = carouselInner.firstChild.className + " active";
 
-            // $(modal-carousel > carousel-inner):first-child.addClass('active');
-
         }
-        //var path = photo.attr('data-photoPath') // Extract info from data-* attributes
-        // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-        // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-        // var modal = $(this);
-        // modal.find('.modal-body img').attr('src', path);
     })
 });
 
