4a9e91d9e361d97ce9882e4b15917c22ea6e2f3f
Andrew Pang <andrew.y.pang@gmail.com>
2019-07-15 18:04:45 -0700

    carousel first slide

--- dist/js/pop.js
+++ dist/js/pop.js
@@ -2,11 +2,36 @@ $(document).ready(function() {
 
     $('#lightboxModal').on('show.bs.modal', function (event) {
         var photo = $(event.relatedTarget) // Button that triggered the modal
-        var path = photo.attr('data-photoPath') // Extract info from data-* attributes
+        console.log(photo);
+
+        var paths = photo.data('photopath');
+        console.log(paths);
+
+        if(paths){
+            paths.split(',').forEach(function(pathForPhoto) {
+
+                var newCaroItem = document.createElement('div');
+                newCaroItem.className = 'carousel-item active';
+                
+                var newImg = document.createElement('img');
+                newImg.src = pathForPhoto;
+                newImg.className = 'd-block w-100';
+
+                newCaroItem.appendChild(newImg);
+
+                document.getElementById('carousel-inner').appendChild(newCaroItem);
+                // $('carousel-inner').appendChild(newCaroItem);
+
+            });
+
+            // $(modal-carousel > carousel-inner):first-child.addClass('active');
+
+        }
+        //var path = photo.attr('data-photoPath') // Extract info from data-* attributes
         // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
         // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-        var modal = $(this);
-        modal.find('.modal-body img').attr('src', path);
+        // var modal = $(this);
+        // modal.find('.modal-body img').attr('src', path);
     })
 });
 
